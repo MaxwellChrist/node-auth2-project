a55ccc2dd7ea2a348850e15dff26a7ae513b2b09
MaxwellChrist <ribthulu@gmail.com>
2022-05-24 18:14:43 -0400

    cleaned up code

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -14,35 +14,9 @@ const restricted = async (req, res, next) => {
     next({ status: 401, message: "Token invalid" });
     return;
   }
-
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
 }
 
 const only = role_name => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
   if(req.decodedJwt == null) {
     next({ message: 'internal server error!' });
     return;
@@ -55,15 +29,7 @@ const only = role_name => (req, res, next) => {
   next()
 }
 
-
 const checkUsernameExists = async (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
   let {username} = req.body;
   let result = await Users.findBy({username})
   if (result.length > 0) {
@@ -74,31 +40,6 @@ const checkUsernameExists = async (req, res, next) => {
 }
 
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
-  // if (req.body.role_name != null) {
-  //   req.body.role_name = req.body.role_name.trim();
-  // } 
-  // if (req.body.role_name.trim() ===  "" || req.body.role_name == null) {
-  //   req.body.role_name = "student";
-  // }
-
   if (req.body.role_name == null || req.body.role_name.trim() ===  "") {
     req.body.role_name = "student";
   } else {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const {   restricted, checkUsernameExists, validateRoleName, only } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+const { JWT_SECRET } = require("../secrets")
 const Users = require('../users/users-model')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
@@ -9,25 +9,12 @@ router.post("/register", validateRoleName, (req, res, next) => {
   let result = req.body
   const hash = bcrypt.hashSync(req.body.password, 8)
   result.password = hash
-
   Users.add(result)
   .then(resultSuccess => {
     res.status(201).json(resultSuccess)
   })
   .catch(next)
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
-});
-
+})
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
   let {username, password} = req.body
@@ -42,25 +29,6 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
   })
   .catch(next)
   })
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
 
 function generateToken(result) {
   const payload = {
