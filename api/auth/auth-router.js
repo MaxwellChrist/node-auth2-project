1e7049cad3fe79afb341249671901475d06f563a
MaxwellChrist <ribthulu@gmail.com>
2022-05-24 18:04:40 -0400

    mvp completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,6 +3,7 @@ const {   restricted, checkUsernameExists, validateRoleName, only } = require('.
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const Users = require('../users/users-model')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   let result = req.body
@@ -29,6 +30,18 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let {username, password} = req.body
+  Users.findBy({username})
+  .then(([result]) => {
+    if (result && bcrypt.compareSync(password, result.password)) {
+      const token = generateToken(result);
+      res.status(200).json({ message: `${result.username} is back!`, token })
+    } else {
+      next({ status: 401, message: 'Invalid Credentials' })
+    }
+  })
+  .catch(next)
+  })
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -48,6 +61,15 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
       "role_name": "admin" // the role of the authenticated user
     }
    */
-});
+
+function generateToken(result) {
+  const payload = {
+    subject: result.user_id,
+    role_name: result.role_name,
+    username: result.username,
+  };
+  const options = { expiresIn: '1d' };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
